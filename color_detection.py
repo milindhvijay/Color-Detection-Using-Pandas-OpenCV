519a0775de15b6603a2d59ec2cdd23d37fbd9ad3
Milindh Vijay <milindhvijay@gmail.com>
2020-04-22 05:33:31 +0530

    Draw Function

--- color_detection.py
+++ color_detection.py
@@ -7,7 +7,7 @@ import argparse
 clicked = False
 r = g = b = xpos = ypos = 0
 
-#Arguement Parser to input image link
+#Argument Parser to input image link
 ap = argparse.ArgumentParser()
 ap.add_argument('-i', '--image', required=True, help="Image Path")
 args = vars(ap.parse_args())
@@ -21,5 +21,26 @@ index = ["color","color_name","hex","R","G","B"]
 csv = pd.read_csv('colors.csv', names=index. header=None)
 
 #Function to calculate minimum distance from all colors
+def getColorName(R,G,B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R- int(csv.lov[i,"R"])) + abs(G- int(csv.loc[i,"G]"])) + abs(B- int(csv.loc[i,"B"]))
+        if(d<=minimum):
+            minimum = d
+            cname = csv.loc[i,"color_name"]
+    return cname
 
 #Function to get xpos and ypos from click
+def draw_function(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b,g,r,xpos,ypos,clicked
+        clicked = True
+        xpos = x
+        ypos = y
+        b,g,r = img[y,x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
+
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',draw_function)
